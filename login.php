1bb9f36b8789fb41c93c3d0e3285b18d55a4a04c
Hawra <hawraaabdulla535@gmail.com>
2024-11-26 22:54:58 +0300

    add naming tags

--- login.php
+++ login.php
@@ -3,19 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
+    <title>User Login</title>    
+    <!-- <link rel="stylesheet" href="styles.css?v=2.0"> -->
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="login_process.php" method="POST">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br><br>
+    <div class="form-container">
+        <h2>Login</h2>
+        <hr>
+        <form action="login_process.php" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" placeholder="Enter Username" name="username" id="username" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" placeholder="Enter Password" name="password" id="password" required><br><br>
 
-        <button type="submit">Login</button>
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a></p>
+            <button type="submit" class="registerbtn">Login</button>
+        </form>
+        <footer>
+            <p><br>Don't have an account? <a href="register.php">Register here</a></p>
+        </footer>
+    </div>
 </body>
 </html>
